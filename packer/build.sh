60dd914d6e463a45bdd38cd1a9827786b1658110
thoth <thoth@mktulu.thalhalla.net>
2014-11-01 17:25:37 -0500

    qemu

--- packer/build.sh
+++ packer/build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-rm -r vendor
-rm Berksfile.lock
-berks vendor vendor/cookbooks
-#/usr/bin/time -v packer build -debug --only=virtualbox-iso pdd-debian-7.7-i386.json
-/usr/bin/time -v packer build --only=virtualbox-iso freeipa-fedora-20-x86_64.json
-# qemu only gives errors, because vagrant cant find the virtualbox image
-#/usr/bin/time -v packer build -debug --only=qemu pdd-debian-7.7-i386.json
-#/usr/bin/time -v packer build --only=qemu pdd-debian-7.7-i386.json
-#/usr/bin/time -v packer build -parallel=false --only=qemu,virtualbox-iso pdd-debian-7.7-i386.json
-#/usr/bin/time -v packer build pdd-debian-7.7-i386.json
+#rm -r vendor
+#rm Berksfile.lock
+#berks vendor vendor/cookbooks
+/usr/bin/time -v packer build --only=qemu freeipa-fedora-20-x86_64.json
 # beeper
-aplay /usr/share/sounds/alsa/Front_Center.wav
+#aplay /usr/share/sounds/alsa/Front_Center.wav
 # ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
