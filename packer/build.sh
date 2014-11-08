2633edf2bf938a8597bd29eab7bfe5b9ae18a8b1
Josh Cox <josh@webhosting.coop>
2014-11-07 18:28:37 -0600

    cookbook cleanup

--- packer/build.sh
+++ packer/build.sh
@@ -2,6 +2,7 @@
 rm -r vendor
 rm Berksfile.lock
 berks vendor vendor/cookbooks
+#/usr/bin/time -v packer build -debug --only=qemu freeipa-fedora-20-x86_64.json
 /usr/bin/time -v packer build --only=qemu freeipa-fedora-20-x86_64.json
 #/usr/bin/time -v packer build --only=virtualbox-iso freeipa-fedora-20-x86_64.json
 #/usr/bin/time -v packer build -debug --only=virtualbox-iso freeipa-fedora-20-x86_64.json
