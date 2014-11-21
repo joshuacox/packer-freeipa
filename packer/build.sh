6eb390174752699c53377c8f3eaaf36628df4e81
Josh Cox <josh@webhosting.coop>
2014-11-20 21:52:33 -0600

    added makefile

--- packer/build.sh
+++ packer/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-rm -r vendor
-rm Berksfile.lock
-berks vendor vendor/cookbooks
+#rm -r vendor
+#rm Berksfile.lock
+#berks vendor vendor/cookbooks
 #/usr/bin/time -v packer build -debug --only=qemu freeipa-fedora-20-x86_64.json
-/usr/bin/time -v packer build --only=qemu freeipa-fedora-20-x86_64.json
+#/usr/bin/time -v packer build --only=qemu freeipa-fedora-20-x86_64.json
 #/usr/bin/time -v packer build --only=virtualbox-iso freeipa-fedora-20-x86_64.json
 #/usr/bin/time -v packer build -debug --only=virtualbox-iso freeipa-fedora-20-x86_64.json
 # beeper
 #aplay /usr/share/sounds/alsa/Front_Center.wav
 #ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
+make oltorf
