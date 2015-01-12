dde91890ebcfa2832fdb948c882878471843fed6
Josh Cox <josh@webhosting.coop>
2015-01-11 18:39:24 -0600

    updates

--- packer/build.sh
+++ packer/build.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-#rm -r vendor
-#rm Berksfile.lock
-#berks vendor vendor/cookbooks
-#/usr/bin/time -v packer build -debug --only=qemu freeipa-fedora-20-x86_64.json
-#/usr/bin/time -v packer build --only=qemu freeipa-fedora-20-x86_64.json
-#/usr/bin/time -v packer build --only=virtualbox-iso freeipa-fedora-20-x86_64.json
-#/usr/bin/time -v packer build -debug --only=virtualbox-iso freeipa-fedora-20-x86_64.json
-# beeper
-#aplay /usr/share/sounds/alsa/Front_Center.wav
-#ogg123 /usr/share/sounds/KDE-Im-Irc-Event.ogg
-make oltorf
+make build-quiet
